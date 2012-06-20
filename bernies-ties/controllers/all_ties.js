63532132962d5e5faf9e159717efa43f0aa71556
Mark Ni <xixian.ni@gmail.com>
2012-06-19 22:48:01 -0700

    moving ties.js to controllers folder and renamed to all_ties.js, so we know which controls which....

--- bernies-ties/controllers/all_ties.js
+++ bernies-ties/controllers/all_ties.js
@@ -1,4 +1,4 @@
-var Tie = require('./models/tie.js');
+var Tie = require('./../models/tie.js');
 
 var data = [{"name":"Blue Steel", "url":"http://img.alibaba.com/img/pb/310/378/300/300378310_920.JPG"},
 			{"name":"Flying Hawaiian", "url":"http://i2i.qinzhi.com/wp-content/uploads/2011/04/Neck-Tie.jpg"}];
