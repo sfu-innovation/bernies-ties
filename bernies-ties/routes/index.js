3b8ea12c717b8cfd20a2bd94157cd75954aca348
Mike Klemarewski <mike.klemarewski@gmail.com>
2012-06-19 20:46:44 -0700

    Adding all ties page

--- bernies-ties/routes/index.js
+++ bernies-ties/routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Bernies Ties' })
 };
+
+exports.allTies = function(req, res){
+  res.render('all_ties', { title: 'All Bernies Ties' })
+};
