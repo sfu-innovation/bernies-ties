cacf6cdfb58b1131dc392270635438ecb984e0e4
Mike Klemarewski <mike.klemarewski@gmail.com>
2012-06-21 14:03:18 -0700

    [#31551721] Database is now queried every time a page is requested

--- bernies-ties/controllers/all_ties.js
+++ bernies-ties/controllers/all_ties.js
@@ -3,7 +3,6 @@ var config = JSON.parse(fs.readFileSync('package.json'));
 var client = require('redis').createClient(config.database.port, config.database.host);
 var Tie = require('../models/tie.js');
 var ties = [];
-var keys = [];
 var randomTie = [];
 
 client.select(config.database["db-num"], function(err,result){
@@ -15,27 +14,40 @@ client.select(config.database["db-num"], function(err,result){
 
 });
 
-client.keys("*", function(err, result){
-	if (err){
-		console.log(err);
-		return;
+
+exports.getTies = function(callback){
+
+	console.log("GETTING TIES");
+	//Get all the keys, then execute a get on each key
+	client.keys("*", function(err, result){
+		var keys = [];
+		var i;
+		var remaining;
+		keys = result;
+		remaining = keys.length;
+
+		if (err){
+			console.log(err);
+			return;
+		}
 
 
-	}
+		for(i = 0; i < keys.length; i++){
+			client.get(keys[i], function(err, a){
+				var json = eval('(' + a + ')');
+				var tie = new Tie.Tie(json);
+				ties.push(tie);
 
-	keys = result;
+				if(--remaining === 0){
+					callback(ties);
+				}
+			});
+		}
 
-	var i;
-	for(i = 0; i < keys.length; i++){
-		client.get(keys[i], function(err, a){
-			var json = eval('(' + a + ')');
-			var tie = new Tie.Tie(json);
-			ties.push(tie);
-		});
-	}
 
-	//This chooses a random tie, but onle once per server start
-	//Still don't know how to stop client on time, so I'll leave this in
+	});
+}
+exports.getRandomTie = function(callback){
 	client.randomkey(function(err,key){
 		if (err){
 			console.log(err);
@@ -48,15 +60,9 @@ client.keys("*", function(err, result){
 				return;
 
 			}
-
 			var json = eval('(' + a + ')');
-			randomTie.push(new Tie.Tie(json));
+			callback(new Tie.Tie(json));
 		});
 
 	});
-
-});
-
-
-exports.ties = ties;
-exports.randomTie = randomTie;
+}
--- bernies-ties/routes/index.js
+++ bernies-ties/routes/index.js
@@ -1,6 +1,7 @@
 var 
 	fs = require('fs'),
 	ties = require('../controllers/all_ties.js').ties;
+	tieList = require('../controllers/all_ties.js');
 var config = JSON.parse(fs.readFileSync('package.json'));
 var client = require('redis').createClient(config.database.port, config.database.host);
 var Tie = require('../models/tie.js');
@@ -13,52 +14,54 @@ client.select(config.database["db-num"], function(){});
 
 exports.index = function(req, res){
 
-	var randomTie = ties[Math.floor(Math.random() * ties.length)]; 
+	tieList.getRandomTie(function(randomTie){
 
-	res.cookie('rememberme', 'yes', { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true });
-	res.render('index', { title: 'Bernies Ties' , tie:randomTie})
+		res.cookie('rememberme', 'yes', { expires: new Date(Date.now() + 900000), httpOnly: true, secure: true });
+		res.render('index', { title: 'Bernies Ties' , tie:randomTie})
 
+	});
 };
 
 exports.allTies = function(req, res){
+	tieList.getTies(function(ties){
+		var tie_id = req.body.id,
+			vote = req.body.vote;
 
-	console.log(ties);
-	var tie_id = req.body.id,
-		vote = req.body.vote;
-
-	var cookie = req.session.cookie;
+		var cookie = req.session.cookie;
 
 
-	for(i in ties) {
-		if (ties[i].attributes.name === tie_id){
-			ties[i].vote(cookie, vote);
-			break;
-		}
-	}
-	
-	//console.log(ties);
-	
-	
-	ties.sort(function(a,b) { 
-		//console.log("a = " + a.attributes.name);
-		//console.log("b = " + b.attributes.name);
-
-		var first_element = a.average();
-		var second_element = b.average();
-
-		if (!first_element) {
-			first_element = 0;
-		}
-
-		if (!second_element) {
-			second_element = 0;
+		for(i in ties) {
+			if (ties[i].attributes.name === tie_id){
+				ties[i].vote(cookie, vote);
+				break;
+			}
 		}
 		
-		return parseFloat(second_element) - parseFloat(first_element) 		
+		//console.log(ties);
+		
+		
+		ties.sort(function(a,b) { 
+			//console.log("a = " + a.attributes.name);
+			//console.log("b = " + b.attributes.name);
+
+			var first_element = a.average();
+			var second_element = b.average();
+
+			if (!first_element) {
+				first_element = 0;
+			}
+
+			if (!second_element) {
+				second_element = 0;
+			}
+			
+			return parseFloat(second_element) - parseFloat(first_element) 		
+		});
+
+
+		res.render('all_ties', { title: 'All Bernie\'s Ties', ties: ties });
+
 	});
-
-
-	res.render('all_ties', { title: 'All Bernie\'s Ties', ties: ties });
 };
 
 exports.uploadTie = function(req, res) {
