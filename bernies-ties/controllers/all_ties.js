c526294a0e0acd97fbfa38b858d6f6aef1ebee74
Mark Ni <xixian.ni@gmail.com>
2012-06-21 19:57:33 -0700

    [#31551415] created search function searchTies().

--- bernies-ties/controllers/all_ties.js
+++ bernies-ties/controllers/all_ties.js
@@ -14,12 +14,35 @@ client.select(config.database["db-num"], function(err,result){
 
 });
 
+// Trying to reuse the getTies code here
 
 exports.getTies = function(callback){
+	getTies("",callback);
+	return;
+}
 
+exports.searchTies = function (keyword,callback){
+	getTies(keyword,callback);
+	return;
+}
+
+
+var getTies = function(keyword,callback){
+	var key;
+	if(!keyword){
+		//if no keyword, we take all the keys
+		key = "*";
+	}
+	else{
+		//otherwise, we use a wildcard to search
+		key = "*"+keyword+"*";
+	}
 	console.log("GETTING TIES");
+
 	//Get all the keys, then execute a get on each key
-	client.keys("*", function(err, result){
+	//Notice: in a real projects, the KEY command should never be used to do search, see http://redis.io/commands/keys for details
+
+	client.keys(key, function(err, result){
 		var keys = [];
 		var i;
 		var remaining;
